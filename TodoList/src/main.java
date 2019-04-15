a3f042c26bd3ef742e830044383b25eb8e4c068d
keitharnao <keitharnao@keiths-air-482.3521.dhcp.asu.edu>
2019-04-14 18:55:41 -0700

    Created frame for to-do list and added some buttons

--- TodoList/src/main.java
+++ TodoList/src/main.java
@@ -1,8 +1,23 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class main {
 
 	public static void main(String[] args) {
-
+		JFrame todoGUI = new JFrame("To-do List");	// frame for To-do List GUI
+		todoListGUI pnl_todoListPanel = new todoListGUI();
+		
+		todoGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	// exit window to close program
+		todoGUI.setSize(700, 450);	// set size of GUI
+		
+		todoGUI.getContentPane().add(pnl_todoListPanel);
+		todoGUI.getContentPane().add(BorderLayout.CENTER, pnl_todoListPanel);
+		
+		todoGUI.setVisible(true);	// makes GUI show up
+		todoGUI.setResizable(false);	// can't resize window
+		todoGUI.setLocationRelativeTo(null);	// centers GUI on screen
 	}
 
 }
--- TodoList/src/todoListGUI.java
+++ TodoList/src/todoListGUI.java
@@ -1,4 +1,55 @@
+import javax.swing.JPanel;
 
-public class todoListGUI {
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+public class todoListGUI extends JPanel {
+
+	private JLabel lbl_TodoList;
+	private JButton btn_New;
+	private JButton btn_Add;
+	private JButton btn_Delete;
+	private JButton btn_Change;
+	private JButton btn_Display;
+	private JButton btn_PrintAll;
+
+	public todoListGUI() {
+
+		this.lbl_TodoList = new JLabel("To-do List");
+
+		this.btn_New = new JButton("NEW");
+		this.btn_Add = new JButton("ADD");
+		this.btn_Delete = new JButton("DELETE");
+		this.btn_Change = new JButton("CHANGE");
+		this.btn_Display = new JButton("DISPLAY");
+		this.btn_PrintAll = new JButton("PRINT ALL");
+
+		// this.btn_CountButton.addActionListener(new ButtonListener());
+
+		// this.lbl_Counter = new JLabel("Pushes: ");
+
+		this.add(this.btn_New);
+		this.add(this.btn_Add);
+		this.add(this.btn_Delete);
+		this.add(this.btn_Change);
+		this.add(this.btn_Display);
+		this.add(this.btn_PrintAll);
+
+		// this.add(this.lbl_Counter);
+
+		this.setPreferredSize(new Dimension(300, 40));
+	}
+
+	/*
+	 * private class ButtonListener implements ActionListener {
+	 * 
+	 * @Override public void actionPerformed(ActionEvent e) { counter++;
+	 * lbl_Counter.setText("Pushes: " + counter); }
+	 * 
+	 * }
+	 */
 }
